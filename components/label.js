bd8385ad467d01daa140c3652e3da98afa7d8d68
antonydeepnz <mashoshini@gmail.com>
2020-02-27 18:03:50 +0300

    1 file modified

--- components/label.js
+++ components/label.js
@@ -6,6 +6,9 @@ const Label = styled.label`
   background-color: transparent;
   color: rgba(red, .75);
   position: absolute;
+  top: 0;
+  left: 0;
+  user-select: none;
   font-size: ${props => props.animate ? "15px" : "20px"};
   transform: ${props => props.animate ? "translate(0em,-1em)" : "translate(0px,0px)"};
   transition: font-size 1s, transform 1s;
