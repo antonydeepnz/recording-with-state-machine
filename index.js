29db3cdf765b67178ce46a3905c5f35dbff2a7d3
antonydeepnz <mashoshini@gmail.com>
2020-03-03 14:13:00 +0300

    2 files modified

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { render } from 'react-dom'
 import './style.css'
 import i18next from 'i18next'
+import axios from 'axios'
 
 import Hello from './components/hellostage'
 
@@ -10,7 +11,15 @@ import form from './components/form'
 
 import Field, {f} from './state-tree/field'
 
+const getData = async () => {
+    const data = await axios.get(`http://www.omdbapi.com/?apikey=2e8a9edb&t=avengers`)
+    await console.log(data)
+  }
+
+
 const App = () => {
+  useEffect(() => (getData()),[])
+
   return(
     <div className={''}>
       <Form form={form} />
